e3b3b090426dc38d311e3fe3a7a5db902c5721a0
TanNguyen <phuctan3108@gmail.com>
2024-02-06 18:00:42 +0100

    Add functions to construct lattice positions
    Generate 1D rings and 2D hexagonal lattices

--- NanocrystalLattice.jl
+++ NanocrystalLattice.jl
@@ -62,5 +62,62 @@ function NanocrystalPositions(nx, ny, nz, length, NCIndex)
   return result
 end
 
+# ------------------------------------------
+"""
+Function that returns a matrix of positions (in 3D) of 2D array of nanocrystals
+of which its plane is parallel to xy-plane and the a_1 primitive vector points 
+along the x direction
+## INPUT
+`a` : length of the primitive translational vectors
+`n1` : number of points along a line parallel to the a_1 primitive vector
+`n2` : number of points along a line parallel to the a_2 primitive vector
+`z_coor` : z coordinates of the plane, zCoor = 0 if the array lies in xy-plane
+"""
+function NanocrystalPositions2DHexagonal(a, n1, n2, z_coor)
+  HALF_FRACION = 1//2
+  SQUARE_ROOT_3 = sqrt(3.0)
+  basis_a1 = [1, 0]
+  basis_a2 = [-HALF_FRACION, HALF_FRACION]
 
+  hex_coordinates = zeros(3, n1*n2)
+  hex_coordinates[3, :] .= z_coor
+  for i2=1:n2
+    for i1=1:n1
+      idx_2d = (i2-1)*n1 + i1
+      hex_coordinates[1, idx_2d] = mod((i1-1)*basis_a1[1] + (i2-1)*basis_a2[1], n1) * a
+      hex_coordinates[2, idx_2d] = ((i1-1)*basis_a1[2] + SQUARE_ROOT_3*((i2-1)*basis_a2[2])) * a
+    end
+  end
+  return hex_coordinates
+end
+
+# ------------------------------------------
+"""
+Function that returns a matrix of positions (in 3D) of nanocrystals arranged 
+in a 1-dimensional ring contained in the xy-plane
+## INPUT
+`N` : total number of nanocrystals in the ring
+`LNC` : average size of nanocrystals
+`Lshell` : shell length
+"""
+function NanocrystalPositions1DRing(n, length, z_coor)
+  ring_pos = zeros(Float64, 3, n)
+  ring_pos[3,:] .= z_coor
+
+  ring_rad = (n//2) *BigFloat(length/pi) 
+  angles = BigFloat(pi) * 2*range(0,n-1)//n
+  
+  ring_pos[1,:], ring_pos[2,:] = ring_rad*cos.(angles), ring_rad*sin.(angles)
+
+  if (n%2 == 0)
+    ring_pos[2, n ÷ 2 + 1] = 0.0
+  end
+
+  if (n%4 == 0)
+    ring_pos[1, n ÷ 4 + 1] = 0.0
+    ring_pos[1, 3 * n ÷ 4 + 1] = 0.0
+  end
+
+  return ring_pos
+end
 
